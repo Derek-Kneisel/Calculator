dd8ca80fe183e96ac8818588f00c01019fc3d9cf
Derek-Kneisel <kneised1@tcnj.edu>
2018-06-12 15:17:27 -0400

    Finished UI and set up code structure

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Enum for calculator modes
+enum modes {
+    case notSet
+    case addition
+    case subtraction
+}
 
+class ViewController: UIViewController {
+    
+    // Instance variables
+    var labelString: String = "0"
+    var currentMode: modes = .notSet
+    var savedNum: Int = 0
+    var lasButtonPressed: Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Updates results label
+    func updateText(){
+    }
+    
+    // Updates modes
+    func changeModes(newMode: modes){
+        
+    }
 
 }
 
