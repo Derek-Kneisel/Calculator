f2ffec81156d8213eda99247ca59f4b0523feea4
Derek-Kneisel <kneised1@tcnj.edu>
2018-06-12 15:41:44 -0400

    Added functionality to number buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,6 +17,9 @@ enum modes {
 
 class ViewController: UIViewController {
     
+    // Results label
+    @IBOutlet weak var label: UILabel!
+    
     // Instance variables
     var labelString: String = "0"
     var currentMode: modes = .notSet
@@ -33,9 +36,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // Addition button
+    @IBAction func didPressPlus(_ sender: Any) {
+    }
+    
+    // Subtraction button
+    @IBAction func didPressSubtract(_ sender: Any) {
+    }
+    
+    // Equals button
+    @IBAction func didPressEquals(_ sender: Any) {
+    }
+    
+    // Clear button
+    @IBAction func didPressClear(_ sender: Any) {
+    }
+    
+    // Number buttons
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        // Gets string value of button name
+        let stringValue: String? = sender.titleLabel?.text
+        
+        // Appends the number pressed to the result string
+        labelString = labelString.appending(stringValue!)
+        
+        // Converts labelString to int
+        updateText()
+    }
+    
     // Updates results label
     func updateText(){
+        // Guard statement for converting string to int
+        guard let labelInt: Int = Int(labelString) else {
+            return
+        }
+        
+        // Updates result label - Assigning it as an int to avoid repeating 0's
+        label.text = "\(labelInt)"
     }
     
     // Updates modes
